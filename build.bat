6f56ee33ff248c1744d17a52becfa579c1ee410a
Adam Heurlin <adamheurlin@gmail.com>
2016-10-06 21:13:20 +0200

    Stop using version specifiers in favour of passing version to compiler

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @echo off
 
+rem Library version
+set VERSION="0.1.0"
+
 rem Path to the compiler
 set OSPC="%OSP%\Osprey\bin\Release\Osprey.exe"
 rem Path to the library folder
@@ -10,4 +13,4 @@ if not exist "%LIB%\testing.unit\" (
 	mkdir "%LIB%\testing.unit"
 )
 
-%OSPC% /libpath "%LIB%" /type module /out "%LIB%\testing.unit\testing.unit.ovm" /doc "%LIB%\testing.unit\testing.unit.ovm.json" /verbose src\testing.unit.osp
+%OSPC% /version %VERSION% /libpath "%LIB%" /type module /out "%LIB%\testing.unit\testing.unit.ovm" /doc "%LIB%\testing.unit\testing.unit.ovm.json" /verbose src\testing.unit.osp
